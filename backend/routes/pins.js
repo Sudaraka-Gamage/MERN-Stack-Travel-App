e0ee4b0a6a872705454e057eca4d3b8e5d93b49b
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-06-02 11:17:05 +0530

    my project

---
+++ backend/routes/pins.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const Pin = require("../models/Pin");
+
+//create a pin
+
+router.post("/", (req,res)=>{
+    const newPin = new Pin(req.body)
+})
+
+//get all pins
