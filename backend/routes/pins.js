5d463cc9d8f1d62c900ad3e64343a041dc9de033
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-06-04 00:32:03 +0530

    Backend connection

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -3,9 +3,25 @@ const Pin = require("../models/Pin");
 
 //create a pin
 
-router.post("/", (req,res)=>{
-    const newPin = new Pin(req.body)
-})
+router.post("/", async (req,res)=>{
+    const newPin = new Pin(req.body);
+    try{
+        const savedPin = await newPin.save();
+        res.status(200).jason(savedPin);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 //get all pins
-       
+
+router.get("/", async (req, res)=>{
+    try{
+        const pins = await Pin.find();
+        res.status(200).json(pins);
+    }catch(err){
+        res.status(500).json(er);
+    }
+});
+
+module.exports = router;
